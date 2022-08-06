9fdf7d03f0eae29e13f6b24e7dbf894646d43943
Fernando Silva <fernandonsilva16@gmail.com>
2022-08-06 12:42:23 -0300

    feat(basics): add bool exercises

--- src/lf/Basics.v
+++ src/lf/Basics.v
@@ -101,3 +101,33 @@ Proof. simpl. reflexivity. Qed.
 
 Example test_orb5: false || false || true = true.
 Proof. simpl. reflexivity. Qed.
+
+Definition nandb (b1: bool) (b2: bool) : bool := 
+  negb (andb b1 b2).
+
+Example test_nandb1: (nandb true false) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_nandb2: (nandb false false) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_nandb3: (nandb false true) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_nandb4: (nandb true true) = false.
+Proof. simpl. reflexivity. Qed.
+
+Definition andb3 (b1: bool) (b2: bool) (b3: bool) : bool :=
+  andb b1 (andb b2 b3).
+
+Example test_andb31: (andb3 true true true) = true.
+Proof. simpl. reflexivity. Qed.
+
+Example test_andb32: (andb3 false true true) = false.
+Proof. simpl. reflexivity. Qed.
+
+Example test_andb33: (andb3 true false true) = false.
+Proof. simpl. reflexivity. Qed.
+
+Example test_andb34: (andb3 true true false) = false.
+Proof. simpl. reflexivity. Qed.
