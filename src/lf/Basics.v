64b38f231234dea5f60056e36cdc0bb9687672bb
Fernando Silva <fernandonsilva16@gmail.com>
2022-08-06 19:27:51 -0300

    feat(basics): add proof by case analysis (#24)

--- src/lf/Basics.v
+++ src/lf/Basics.v
@@ -436,3 +436,58 @@ Proof.
   rewrite <- mult_n_Sm.
   rewrite <- mult_n_O.
   reflexivity. Qed.
+
+Theorem plus_1_neq_0_firsttry : forall n : nat,
+  (n + 1) =? 0 = false.
+Proof.
+  intros n. destruct n as [| n'] eqn : E.
+  - reflexivity.
+  - reflexivity. Qed.
+
+Theorem negb_involutive : forall b : bool,
+  negb (negb b) = b.
+Proof.
+  intros b. destruct b eqn : E.
+  - reflexivity.
+  - reflexivity. Qed.
+
+Theorem andb_commutative : forall b c, andb b c = andb c b.
+Proof.
+  intros b c. destruct b eqn : Eb.
+  - destruct c eqn : Ec.
+    + reflexivity.
+    + reflexivity.
+  - destruct c eqn : Ec.
+    + reflexivity.
+    + reflexivity.
+Qed.
+
+Theorem andb_commutative' : forall b c, andb b c = andb c b.
+Proof.
+  intros b c. destruct b eqn : Eb.
+  { destruct c eqn : Ec.
+    { reflexivity. }
+    { reflexivity. } }
+  { destruct c eqn : Ec.
+    { reflexivity. }
+    { reflexivity. } }
+Qed.
+
+Theorem andb3_exchange : forall b c d, andb (and b c) d = andb (andb b d) c.
+Proof.
+  intros b c d. destruct b eqn : Eb.
+  - destruct c eqn : Ec.
+    { destruct d eqn : Ed.
+      - reflexivity.
+      - reflexivity. }
+    { destruct d eqn : Ed.
+      - reflexivity.
+      - reflexivity. }
+  - destruct c eqn : Ec.
+    { destruct d eqn : Ed.
+      - reflexivity.
+      - reflexivity. }
+    { destruct d eqn : Ed.
+      - reflexivity.
+      - reflexivity. }
+Qed.
