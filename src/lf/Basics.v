bb604bfab7ec991aca3bcd75ca695aa7dd6264a7
Fernando Silva <fernandonsilva16@gmail.com>
2022-08-06 13:09:58 -0300

    feat(basics): add tuples (#19)

--- src/lf/Basics.v
+++ src/lf/Basics.v
@@ -191,3 +191,25 @@ Definition b : bool := true.
 
 Check Playground.b : rgb.
 Check b : bool.
+
+Module TuplePlayground.
+  Inductive bit : Type :=
+    | B0
+    | B1.
+
+  Inductive nybble : Type :=
+    | bits (b0 b1 b2 b3 : bit).
+  
+  Check (bits B1 B0 B1 B0)
+    : nybble.
+
+  Definition all_zero (nb : nybble) : bool :=
+    match nb with
+    | bits B0 B0 B0 B0 => true
+    | _ => false
+    end.
+
+  Compute (all_zero (bits B1 B0 B1 B0)).
+
+  Compute (all_zero (bits B0 B0 B0 B0)).
+End TuplePlayground.
