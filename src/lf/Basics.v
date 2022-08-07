a94d0e143d7a8685dc9ede13d250da684d4f1a3d
Fernando Silva <fernandonsilva16@gmail.com>
2022-08-07 00:37:20 -0300

    feat(basics): add final exercises (#29)

--- src/lf/Basics.v
+++ src/lf/Basics.v
@@ -542,3 +542,73 @@ Fixpoint plus' (n m : nat) : nat :=
   | O => m
   | S n' => S (plus' n' m)
   end.
+
+Theorem identity_fn_applied_twice :
+  forall (f : bool -> bool), (forall (x : bool), f x = x) ->
+  forall (b : bool), f (f b) = b.
+Proof.
+  intros f H b.
+  rewrite -> H.
+  rewrite -> H.
+  reflexivity.
+Qed.
+
+Theorem negation_fn_applied_twice :
+  forall (f : bool -> bool), (forall (x : bool), f x = negb x) ->
+  forall (b : bool), f (f b) = b.
+Proof.
+  intros f H b.
+  rewrite -> H.
+  rewrite -> H.
+  destruct b eqn : Eb.
+  - reflexivity.
+  - reflexivity.
+Qed.
+
+Theorem andb_eq_orb :
+  forall (b c : bool), (andb b c = orb b c) ->
+  b = c.
+Proof.
+  intros [] c.
+  - simpl. intros H. rewrite <- H. reflexivity.
+  - simpl. intros H. rewrite <- H. reflexivity.
+Qed.
+
+Inductive bin : Type :=
+  | Z
+  | B0 (n : bin)
+  | B1 (n : bin).
+
+Fixpoint incr (b : bin) : bin :=
+  match b with
+  | Z => B1 Z
+  | B0 b' => B1 b'
+  | B1 b' => B0 (incr b')
+  end.
+
+Fixpoint bin_to_nat (b : bin) : nat :=
+  match b with
+  | Z => 0
+  | B0 b' => 2 * (bin_to_nat b')
+  | B1 b' => 1 + 2 * (bin_to_nat b')
+  end.
+
+Example test_bin_incr1 : (incr (B1 Z)) = B0 (B1 Z).
+Proof. simpl. reflexivity. Qed.
+
+Example test_bin_incr2 : (incr (B0 (B1 Z))) = B1 (B1 Z).
+Proof. simpl. reflexivity. Qed.
+
+Example test_bin_incr3 : (incr (B1 (B1 Z))) = B0 (B0 (B1 Z)).
+Proof. simpl. reflexivity. Qed.
+
+Example test_bin_incr4 : bin_to_nat (B0 (B1 Z)) = 2.
+Proof. simpl. reflexivity. Qed.
+
+Example test_bin_incr5 :
+        bin_to_nat (incr (B1 Z)) = 1 + bin_to_nat (B1 Z).
+Proof. simpl. reflexivity. Qed.
+
+Example test_bin_incr6 :
+        bin_to_nat (incr (incr (B1 Z))) = 2 + bin_to_nat (B1 Z).
+Proof. simpl. reflexivity. Qed.
