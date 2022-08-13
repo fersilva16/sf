b2ad469c6f334d34315c368a5d3a1abed27e9dfb
Fernando Silva <fernandonsilva16@gmail.com>
2022-08-13 12:16:39 -0300

    feat(induction): add basic induction exercise

--- src/lf/Induction.v
+++ src/lf/Induction.v
@@ -13,3 +13,34 @@ Proof.
   - simpl. reflexivity.
   - simpl. rewrite -> IHn'. reflexivity.
 Qed.
+
+Theorem mult_0_r : forall n : nat, n * 0 = 0.
+Proof.
+  intros n. induction n as [| n' IHn'].
+  - reflexivity.
+  - simpl. rewrite -> IHn'. reflexivity.
+Qed.
+
+Theorem plus_n_Sm : forall n m : nat, S (n + m) = n + (S m).
+Proof.
+  intros n m. induction n as [| n' IHn' ].
+  - reflexivity.
+  - destruct m.
+    + simpl. rewrite -> IHn'. reflexivity.
+    + simpl. rewrite -> IHn'. reflexivity.
+Qed.
+
+Theorem add_comm : forall n m : nat, n + m = m + n.
+Proof.
+  intros n m. induction n as [| n' IHn' ].
+  - simpl. rewrite -> add_0_r. reflexivity.
+  - simpl. rewrite <- plus_n_Sm. rewrite -> IHn'. reflexivity.
+  Qed.
+
+Theorem add_assoc : forall n m p : nat, n + (m + p) = (n + m) + p.
+Proof.
+  intros n m p. induction n as [| n' IHn' ].
+    - reflexivity.
+    - simpl. rewrite -> IHn'. reflexivity. 
+Qed.
+
