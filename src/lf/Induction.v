333c546dcf3978857c2b894874395864138fc15f
Fernando Silva <fernandonsilva16@gmail.com>
2022-08-13 12:17:27 -0300

    refactor(induction): improve basic induction exercise

--- src/lf/Induction.v
+++ src/lf/Induction.v
@@ -25,9 +25,7 @@ Theorem plus_n_Sm : forall n m : nat, S (n + m) = n + (S m).
 Proof.
   intros n m. induction n as [| n' IHn' ].
   - reflexivity.
-  - destruct m.
-    + simpl. rewrite -> IHn'. reflexivity.
-    + simpl. rewrite -> IHn'. reflexivity.
+  - simpl. rewrite -> IHn'. reflexivity.
 Qed.
 
 Theorem add_comm : forall n m : nat, n + m = m + n.
@@ -35,7 +33,7 @@ Proof.
   intros n m. induction n as [| n' IHn' ].
   - simpl. rewrite -> add_0_r. reflexivity.
   - simpl. rewrite <- plus_n_Sm. rewrite -> IHn'. reflexivity.
-  Qed.
+Qed.
 
 Theorem add_assoc : forall n m p : nat, n + (m + p) = (n + m) + p.
 Proof.
