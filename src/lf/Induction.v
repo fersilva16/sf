f172dac974563e26de4826d76c00a88b1e479c62
Fernando Silva <fernandonsilva16@gmail.com>
2022-08-13 10:04:44 -0300

    feat(induction): add induction

---
+++ src/lf/Induction.v
@@ -0,0 +1,15 @@
+From LF Require Export Basics.
+
+Theorem add_0_r : forall n : nat, n + 0 = n.
+Proof.
+  intros n. induction n as [| n' IHn'].
+  - reflexivity.
+  - simpl. rewrite -> IHn'. reflexivity.
+Qed.
+
+Theorem minus_n_n : forall n, minus n n = 0.
+Proof.
+  intros n. induction n as [| n' IHn'].
+  - simpl. reflexivity.
+  - simpl. rewrite -> IHn'. reflexivity.
+Qed.
