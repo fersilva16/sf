a1ea97470ad1b8a743f8e882583644d1c8684c9b
Fernando Silva <fernandonsilva16@gmail.com>
2022-08-13 12:21:17 -0300

    feat(induction): add double plus exercise

--- src/lf/Induction.v
+++ src/lf/Induction.v
@@ -42,3 +42,15 @@ Proof.
     - simpl. rewrite -> IHn'. reflexivity. 
 Qed.
 
+Fixpoint double (n : nat) :=
+  match n with
+  | O => O
+  | S n' => S (S (double n'))
+  end.
+
+Lemma double_plus : forall n, double n = n + n.
+Proof.
+  intros n. induction n as [| n' IHn' ].
+  - reflexivity.
+  - simpl. rewrite -> IHn'. rewrite -> plus_n_Sm. reflexivity. 
+Qed.
