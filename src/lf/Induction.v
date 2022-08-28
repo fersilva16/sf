0b495e49b7114edc89000fe7b26947923dbca539
Fernando Silva <fernandonsilva16@gmail.com>
2022-08-28 16:32:08 -0300

    feat(induction): add more exercises (#41)

--- src/lf/Induction.v
+++ src/lf/Induction.v
@@ -152,3 +152,69 @@ Proof.
   - simpl. rewrite -> H. reflexivity.
   - simpl. rewrite -> IHp'. reflexivity.   
 Qed.
+
+Theorem leb_refl : forall n : nat, (n <=? n) = true.
+Proof.
+  intros n. induction n as [| n' IHn' ].
+  - reflexivity.
+  - simpl. rewrite -> IHn'. reflexivity.
+Qed.
+
+Theorem zero_neqb_S : forall n : nat, 0 =? (S n) = false.
+Proof.
+  intros n. reflexivity.
+Qed.
+
+Theorem andb_false_r : forall b : bool, andb b false = false.
+Proof.
+  intros b. destruct b.
+    - reflexivity.
+    - reflexivity.
+Qed.
+
+Theorem S_neqb_0 : forall n : nat, (S n) =? 0 = false.
+Proof.
+  intros n. reflexivity.
+Qed.
+
+Theorem mult_1_l : forall n : nat, 1 * n = n.
+Proof.
+  intros n. destruct n.
+  - reflexivity.
+  - simpl. rewrite -> add_0_r. reflexivity.
+Qed.
+
+Theorem all3_spec : forall b c : bool,
+  orb
+    (andb b c)
+    (orb (negb b)
+         (negb c))
+  = true.
+Proof.
+  intros b c. destruct b.
+    - destruct c.
+      + reflexivity.
+      + reflexivity.
+    - destruct c.
+      + reflexivity.
+      + reflexivity.
+Qed.
+
+Theorem mult_plus_distr_r : forall n m p : nat,
+  (n + m) * p = (n * p) + (m * p).
+Proof.
+  intros n m p. induction n as [| n' IHn' ].
+  - reflexivity.
+  - simpl. rewrite -> IHn'. rewrite -> add_assoc. reflexivity. 
+Qed.
+
+Theorem mult_assoc : forall n m p : nat,
+  n * (m * p) = (n * m) * p.
+Proof.
+  intros n m p. induction n as [| n' IHn' ].
+  - reflexivity.
+  - simpl.
+    rewrite -> IHn'.
+    rewrite -> mult_plus_distr_r.
+    reflexivity.
+Qed.
